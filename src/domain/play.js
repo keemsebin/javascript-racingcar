9c1596b8c6e05cb4c01953bfc37ffb6def7ada42
keemsebeen <sebin0580@gmail.com>
2025-02-14 18:34:35 +0900

    refactor(play): 이동 가능 여부 함수 분리

--- src/domain/play.js
+++ src/domain/play.js
@@ -2,10 +2,14 @@ import { MAX_MOVABLE_NUMBER } from "../constants/race.js";
 import { getMaxPosition } from "../utils/findMax.js";
 import { getRandomNumber } from "../utils/random.js";
 
+export const isMoveValid = (randomNum) => {
+  return randomNum >= MAX_MOVABLE_NUMBER;
+};
+
 export const playRound = (cars) => {
   return cars.map((car) => {
     const randomNum = getRandomNumber();
-    if (randomNum >= MAX_MOVABLE_NUMBER) {
+    if (isMoveValid(randomNum)) {
       return { ...car, position: car.position + 1 };
     }
     return car;
