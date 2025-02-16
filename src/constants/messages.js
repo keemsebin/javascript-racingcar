cb301e991cf2027b7bc0b58b138314c5213bbb7d
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:23:41 +0900

    refactor(constants): ERROR prefix 추가, IS_TRY_COUNT_EMPTY 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,4 @@
-import { MAX_CAR_NAME_LENGTH, MIN_TRY_COUNT_NUM } from "./race.js";
+import { CAR_NAME, MIN_TRY_COUNT_NUM } from "./race.js";
 
 export const INPUT = Object.freeze({
   CAR_NAMES: `경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n`,
@@ -10,10 +10,13 @@ export const OUTPUT = Object.freeze({
   FINAL_WINNER: "최종 우승자:",
 });
 
+export const ERROR_PREFIX = "[ERROR]";
+
 export const ERROR = Object.freeze({
-  IS_CAR_NAME_EMPTY: "자동차 이름이 비어있습니다.",
-  IS_LENGTH_LONGER_THAN_FIVE: `자동차 이름은 ${MAX_CAR_NAME_LENGTH}글자 이내로 작성주세요.`,
-  IS_TRY_COUNT_EMPTY: "시도할 횟수가 비어있습니다.",
-  IS_NOT_ZERO: `${MIN_TRY_COUNT_NUM} 이상의 숫자를 입력해주세요.`,
-  IS_NOT_NUMBER: "숫자를 입력해주세요.",
+  IS_CAR_NAME_EMPTY: `${ERROR_PREFIX} 자동차 이름이 비어있습니다.\n`,
+  IS_LENGTH_LONGER_THAN_FIVE: `${ERROR_PREFIX} 자동차 이름은 ${CAR_NAME.MAX_LENGTH}글자 이내로 작성주세요.\n`,
+  IS_DUPLICATE_CAR_NAME: `${ERROR_PREFIX} 자동차 이름에 중복이 존재합니다.\n`,
+  IS_TRY_COUNT_EMPTY: `${ERROR_PREFIX} 시도할 횟수가 비어있습니다.\n`,
+  IS_NOT_ZERO: `${ERROR_PREFIX} ${MIN_TRY_COUNT_NUM} 이상의 숫자를 입력해주세요.\n`,
+  IS_NOT_NUMBER: `${ERROR_PREFIX} 숫자를 입력해주세요.\n`,
 });
