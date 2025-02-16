f7274b8f1aa74974720c5d1305941fd864034de7
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:32:54 +0900

    refactor(index): retryInput을 활용해 carNames 입력 로직 분리

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 import { INPUT, OUTPUT } from "./constants/messages.js";
+import { DELIMITER } from "./constants/race.js";
 import { createCars } from "./domain/car.js";
-import { getWinnersByPosition, playRacing } from "./domain/play.js";
-import { retryInput } from "./utils/retryInput.js";
+import { playRacing } from "./domain/play.js";
+import { findWinners } from "./domain/winner.js";
 import { splitByDelimiter } from "./utils/split.js";
-import { validateCarNames, validateCount } from "./utils/validation.js";
-import { readLineAsync } from "./view/input.js";
+import { validateCarNames } from "./validation/carNameValidate.js";
+import { validateCount } from "./validation/tryCountValidate.js";
+import { readLineAsync, retryInput } from "./view/input.js";
 import { print, printProcess, printWinners } from "./view/output.js";
 
 async function run() {
-  let carNames, tryNumber;
-
-  await retryInput(async () => {
-    const stringOfCarNames = await readLineAsync(INPUT.CAR_NAMES);
-    carNames = splitByDelimiter(stringOfCarNames, ",");
+  const carNames = await retryInput(async () => {
+    const getCarNamesInput = await readLineAsync(INPUT.CAR_NAMES);
+    const carNames = splitByDelimiter(getCarNamesInput, DELIMITER);
     validateCarNames(carNames);
+    return carNames;
   });
 
-  await retryInput(async () => {
-    tryNumber = await readLineAsync(INPUT.TRY_COUNT);
-    validateCount(tryNumber);
+  const tryCount = await retryInput(async () => {
+    const getTryCount = await readLineAsync(INPUT.TRY_COUNT);
+    validateCount(getTryCount);
+    return getTryCount;
   });
 
   const cars = createCars(carNames);
 
   print(OUTPUT.RESULT);
-  const playResult = playRacing(cars, tryNumber);
+  const playResult = playRacing(cars, tryCount);
   playResult.forEach(printProcess);
 
-  const winners = getWinnersByPosition(playResult);
+  const winners = findWinners(playResult);
   printWinners(winners);
 }
 
