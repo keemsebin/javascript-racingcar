10ac481e9df258f4c7247e239e7292067560d913
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:26:58 +0900

    refactor(output): RACE_MOVE.FORWARD_STR 상수로 변경

--- src/view/output.js
+++ src/view/output.js
@@ -1,17 +1,18 @@
 import { OUTPUT } from "../constants/messages.js";
+import { RACE_MOVE } from "../constants/race.js";
 export const print = (str) => {
   return console.log(str);
 };
 
 export const printProcess = (cars) => {
   cars.forEach((car) => {
-    console.log(`${car.name} :  ${"-".repeat(car.position)}`);
+    console.log(`${car.name} :  ${RACE_MOVE.FORWARD_STR.repeat(car.position)}`);
   });
   console.log("");
 };
 
 export const printWinners = (winners) => {
   console.log(
-    `${OUTPUT.FINAL_WINNER} ${winners.map((winner) => winner.name).join(", ")}`
+    `${OUTPUT.FINAL_WINNER} ${winners.map((winner) => winner.name).join(", ")}`,
   );
 };
