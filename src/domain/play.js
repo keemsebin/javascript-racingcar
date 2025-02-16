e7f0d058ff2f26373b935e21ed6c4b5a67e67a71
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:28:55 +0900

    refactor(play): 각 라운드 별 업데이트 함수 분리, 전진 여부 결정 함수 분리

--- src/domain/play.js
+++ src/domain/play.js
@@ -1,26 +1,31 @@
-import { MAX_MOVABLE_NUMBER } from "../constants/race.js";
-import { getMaxPosition } from "../utils/findMax.js";
+import { RACE_MOVE } from "../constants/race.js";
 import { getRandomNumber } from "../utils/random.js";
 
-export const playRound = (cars) => {
-  return cars.map((car) => {
-    const randomNum = getRandomNumber();
-    if (randomNum >= MAX_MOVABLE_NUMBER) {
-      return { ...car, position: car.position + 1 };
-    }
-    return car;
-  });
+export const getNextCarPosition = (position) => {
+  const randomNumber = getRandomNumber();
+  if (randomNumber >= RACE_MOVE.THRESHOLD) {
+    return position + 1;
+  }
+  return position;
 };
 
-export const playRacing = (cars, tryCount) => {
-  return Array.from({ length: tryCount }).reduce((rounds, _) => {
-    const newRound = playRound(rounds[rounds.length - 1] || cars);
+export const updateCarPosition = (car, moveResult) => ({
+  ...car,
+  position: moveResult,
+});
+
+export const moveCarForward = (car) => {
+  const newPosition = getNextCarPosition(car.position);
+  return updateCarPosition(car, newPosition);
+};
+
+export const playRounds = (cars) => {
+  return cars.map((car) => moveCarForward(car));
+};
+
+export const playRacing = (initialCars, tryCount) => {
+  return Array.from({ length: tryCount }).reduce((rounds) => {
+    const newRound = playRounds(rounds.at(-1) || initialCars);
     return [...rounds, newRound];
   }, []);
-};
-
-export const getWinnersByPosition = (cars) => {
-  const carPlayResult = cars.at(-1);
-  const maxPosition = getMaxPosition(carPlayResult);
-  return carPlayResult.filter((car) => car.position === maxPosition);
 };
