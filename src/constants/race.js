b5f35e1aaa6f5fb86092c439a0593e4a4d899fe5
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:31:32 +0900

    refactor(constants): 기존 상수 MAX_MOVABLE_NUMBER, MAX_CAR_NAME_LENGTH 분리

--- src/constants/race.js
+++ src/constants/race.js
@@ -1,3 +1,13 @@
-export const MAX_MOVABLE_NUMBER = 4;
-export const MAX_CAR_NAME_LENGTH = 5;
+export const RACE_MOVE = {
+  THRESHOLD: 4,
+  FORWARD_STR: "-",
+};
+
+export const CAR_NAME = {
+  MIN_LENGTH: 0,
+  MAX_LENGTH: 5,
+};
+
 export const MIN_TRY_COUNT_NUM = 0;
+
+export const DELIMITER = ",";
