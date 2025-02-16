cda3f73a1fb9d179bace75ca602344202c8a33b9
keemsebeen <sebin0580@gmail.com>
2025-02-17 02:11:37 +0900

    chore: eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  arrowParens: "always",
+  bracketSpacing: true,
+  proseWrap: "never",
+};
