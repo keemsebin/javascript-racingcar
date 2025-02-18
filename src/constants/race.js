0c26002950bd6758a60f9d9318e4067c634b5da5
keemsebeen <sebin0580@gmail.com>
2025-02-18 14:30:05 +0900

    refactor(winner): car property 상수화

--- src/constants/race.js
+++ src/constants/race.js
@@ -7,6 +7,11 @@ export const CAR_NAME = Object.freeze({
   MAX_LENGTH: 5,
 });
 
+export const CAR_PROPERTY = Object.freeze({
+  NAME: "name",
+  POSITION: "position",
+});
+
 export const TRY_COUNT = Object.freeze({
   MIN_NUM: 0,
 });
--- src/domain/winner.js
+++ src/domain/winner.js
@@ -1,7 +1,8 @@
+import { CAR_PROPERTY } from "../constants/race.js";
 import { findMaxValue } from "../utils/findMax.js";
 
 export const findWinners = (playResult) => {
   const finalRound = playResult.at(-1);
-  const maxPosition = findMaxValue(finalRound, "position");
+  const maxPosition = findMaxValue(finalRound, CAR_PROPERTY.POSITION);
   return finalRound.filter((car) => car.position === maxPosition);
 };
